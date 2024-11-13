5052d9939dbd9d1e7665fd1a07d2e47e0b46ed95
Arpit Kumar Parwar <akumarparwar@gmail.com>
2024-11-13 17:55:17 +0530

    Add new folder

--- quiz-app/vite.config.js
+++ quiz-app/vite.config.js
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  root: './', // Ensure Vite starts from the root folder
+  root: './',
   build: {
-    outDir: 'build', // Output directory for the build
-  }
-});
-=======
-// vite.config.js
-export default {
-  build: {
-    outDir: 'build', // Ensure Vercel looks for this directory
+    outDir: 'build', // This is where your build files will be placed
   },
-};
->>>>>>> a4c31a49067df4521d6ed12041e95311916cd1b7
+});
